f31615c7583d1681353a2370812669abef09af52
Siddhi <siddhimahadik257@gmail.com>
2025-03-16 11:00:47 +0530

    Changed search bar button

--- web-app/src/components/search/search.js
+++ web-app/src/components/search/search.js
@@ -5,12 +5,12 @@ export const SearchBar = () => {
     return (
         <div className="searchBar">
 
-            <Button style={{ fontWeight: "var(--fontLWeight)", color: "var(--textDark)", borderRight: "1px solid var(--borderColor)" }}
+            <Button style={{ fontWeight: "var(--fontLWeight)", padding: "0px 15px", color: "var(--textDark)", borderRight: "1px solid var(--borderColor)" }}
 
                 text="Anywhere" variant="default" />
-            <Button style={{ fontWeight: "var(--fontLWeight)", color: "var(--textDark)", borderRight: "1px solid var(--borderColor)" }}
+            <Button style={{ fontWeight: "var(--fontLWeight)", padding: "0px 15px", color: "var(--textDark)", borderRight: "1px solid var(--borderColor)" }}
                 text="Any week" variant="default" />
-            <Button style={{ fontWeight: "var(--fontLWeight)", color: "var(--secondaryColor)" }} text="Add Guests" variant="default" />
+            <Button style={{ fontWeight: "var(--fontLWeight)", padding: "0px 15px", color: "var(--secondaryColor)" }} text="Add Guests" variant="default" />
             <Button variant='search' icon={<i class="fi fi-br-search"></i>} />
 
 
