a00b05e1a2e15c0455d36f1eab8a3a3484f19d6d
Siddhi <siddhimahadik257@gmail.com>
2025-03-18 16:27:12 +0530

    Created chip in the card

--- web-app/src/components/card/card.js
+++ web-app/src/components/card/card.js
@@ -1,9 +1,27 @@
+import { Button } from '../button/button'
 import './card.css'
 
 export const SingleCard = ({ image, location, distance, dates, price, rating }) => {
     return (
         <div className="singleCard">
             <img src={image} alt={location} className="cardImage" />
+            <Button
+                size={"xSmall"}
+                style={{
+                    position: "absolute",
+                    top: "15px",
+                    left: "15px",
+                    background: "var(--backgroundLight)",
+                    color: "var(--textDark)",
+                    fontSize: "var(--fontMedium)",
+                    fontWeight: "var(--fontLWeight)",
+                    borderRadius: "var(--radiusXLarge)",
+                    boxShadow: "0 2px 8px rgba(0, 0, 0, 0.15)",
+                    zIndex: "1"
+                }}
+                text={"Guest favourite"} />
+            <i class="fi fi-rs-heart"></i>
+
             <div className="cardDetails">
                 <div className="locationStar">
                     <p>{location}</p>
